ba767a0ce205fef27380a70175e1250a1aea6c1e
moeageli22 <mohammadmunir2004@gmail.com>
2025-11-16 20:58:47 +0000

    Wellbeing page

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -91,7 +91,7 @@ export default function HomePage() {
                 <ul className="nav-links">
                     <li><a href="#home">Home</a></li>
                     <li><a href="#hotels" onClick={(e) => { e.preventDefault(); navigate('/hotels') }}>Hotels</a></li>
-                    <li><a href="#destinations">Destinations</a></li>
+                    <li><a href="#destinations">Wellbeing</a></li>
                     <li><a href="#groups">Groups</a></li>
                     <li><a href="#about">About</a></li>
                 </ul>
